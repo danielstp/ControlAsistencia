6c5c9fbd53006ea4e015c17e41be0875bacf0e26
jorgegus100 <jalameda100@gmail.com>
2014-09-23 22:46:09 -0400

    adicion de filtro

--- ControlAsistencia/urls.py
+++ ControlAsistencia/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from ControlAsistencia import views
 
 urlpatterns = patterns('',
-  url(r'^diario$', views.diario, name='diario'),
+
+  url(r'^diario/$', views.diario, name='diario'),
   url(r'^mes$', views.mes, name='mes'),
   url(r'^$', views.index, name='index'),
 #  url(r'^$', views.index, name='index'),
--- ControlAsistencia/views.py
+++ ControlAsistencia/views.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django import forms
 from ControlAsistencia.models import Estudiante, Centro
 
 import ControlAsistencia.views
-import django_tables2 as tables
 
+
+ #widget=forms.Select(choices=Centro.objects.all())
 
 def index(request):
   return render(request, 'Opciones.html')
 
 def diario(request):
-  lista = []
-  return render(request, 'RepDiario.html', {'estudiantes': Estudiante.objects.all(), 'centros': Centro.objects.all(), 'lista':lista})
+  if request.GET.get('centros'):
+    estudiantes= Estudiante.objects.filter(centro=request.GET.get('centros'))
+  else:
+    estudiantes= Estudiante.objects.all()
+
+  return render(request, 'RepDiario.html',
+                    {'estudiantes': Estudiante.objects.all(),
+                     'centros': Centro.objects.all(),
+                     'centro':request.GET.get('centros')})
+
+def diario1(request, centro):
+  cen=centro
+  return HttpResponse("You're looking at the results of poll %s." % cen)
+
 
 def mes(request):
   return render(request, 'RepMensual.html', {'estudiantes': Estudiante.objects.all()})
